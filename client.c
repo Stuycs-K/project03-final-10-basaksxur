878ce616f765da7a7affb2a5c8827f68571d4d36
Souvik Basak <souvik.b416@gmail.com>
2025-01-07 15:18:30 -0500

    copied client from lab

--- client.c
+++ client.c
@@ -5,10 +5,29 @@
 #include <stdlib.h>
 #include <time.h>
 
-void sigint_handler(int signo) {
+int to_server = -1;
+int from_server = -1;
 
+static void sighandler(int signo) {
+    if (signo == SIGINT) {
+        if (to_server != -1) {
+            close(to_server);
+        }
+        if (from_server != -1) {
+            close(from_server);
+        }
+        exit(0);
+    }
 }
 
 int main(){
-
+    signal(SIGINT, sighandler);
+    signal(SIGPIPE, sighandler);
+    from_server = client_handshake( &to_server );
+    int random;
+    while (read(from_server, &random, sizeof(int))) {
+//        printf("Random number: %d\n", random);
+    }
+    close(to_server);
+    close(from_server);
 }
