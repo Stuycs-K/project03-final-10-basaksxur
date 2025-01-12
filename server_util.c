ac6930e5c71aa0b4f07fadb0241bb6dc3db62b9a
Souvik Basak <souvik.b416@gmail.com>
2025-01-12 11:19:34 -0500

    created server_util file for userdata and wrote loadUser

---
+++ server_util.c
@@ -0,0 +1,17 @@
+#include "server_util.h"
+
+//returns a user specified by username, if the user doesn't exist returns NULL
+struct user *loadUser(char *username, int dataFile) {
+    struct user *output;
+    char name[20];
+    int offset = 0;
+    while (read(dataFile, name, sizeof(name))) {
+        if (!strcmp(username, name)) {
+            read(dataFile, output, sizeof(struct user));
+            return output;
+        }
+        offset += sizeof(struct user);
+        lseek(dataFile, offset, SEEK_SET);
+    }
+    return NULL;
+}
---
+++ server_util.h
@@ -0,0 +1,16 @@
+#ifndef SERVER_UTIL_H
+#define SERVER_UTIL_H
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+struct user {
+    char username[20];
+    int rating;
+    int gamesPlayed;
+    int gamesWon;
+    int gamesLost;
+    char rank[10];
+    int connected;
+};
+struct user *loadUser(char *username, int dataFile);
+#endif
