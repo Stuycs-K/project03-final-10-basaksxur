20760c027ff6860da195fb16dc6d21396d26677e
Souvik Basak <souvik.b416@gmail.com>
2025-01-12 11:50:54 -0500

    wrote createUser and fixed/tested loadUser

--- server_util.c
+++ server_util.c
@@ -1,17 +1,25 @@
 #include "server_util.h"
 
+struct user *createUser(char username[], int rating, int gamesPlayed, int gamesWon, int gamesLost, char rank[], int connected) {
+    struct user *newUser = malloc(sizeof(struct user));
+    strcpy(newUser->username, username);
+    newUser->rating = rating;
+    newUser->gamesPlayed = gamesPlayed;
+    newUser->gamesWon = gamesWon;
+    newUser->gamesLost = gamesLost;
+    strcpy(newUser->rank, rank);
+    newUser->connected = connected;
+    return newUser;
+}
+
 //returns a user specified by username, if the user doesn't exist returns NULL
 struct user *loadUser(char *username, int dataFile) {
-    struct user *output;
-    char name[20];
-    int offset = 0;
-    while (read(dataFile, name, sizeof(name))) {
-        if (!strcmp(username, name)) {
-            read(dataFile, output, sizeof(struct user));
+    struct user *output = malloc(sizeof(struct user));
+    lseek(dataFile, 0, SEEK_SET);
+    while (read(dataFile, output, sizeof(struct user))) {
+        if (!strcmp(username, output->username)) {
             return output;
         }
-        offset += sizeof(struct user);
-        lseek(dataFile, offset, SEEK_SET);
     }
     return NULL;
 }
